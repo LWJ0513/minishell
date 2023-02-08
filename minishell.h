2ab2696536566b5ffe21c05545c30c0fbaf3356b
임원정 <wonlim@student.42seoul.kr>
2023-02-08 22:59:27 +0900

    edit header

--- minishell.h
+++ minishell.h
@@ -6,17 +6,16 @@
 /*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 21:36:54 by wonlim            #+#    #+#             */
-/*   Updated: 2023/02/08 22:13:53 by wonlim           ###   ########.fr       */
+/*   Updated: 2023/02/08 22:58:35 by wonlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-// # include "./readline/history.h"
-// # include "./readline/readline.h"
-# include <readline/history.h>
-# include <readline/readline.h>
+# include "readline/history.h"
+# include "readline/readline.h"
+# include "libft/libft.h"
 # include <signal.h>
 # include <stdio.h>
 # include <stdlib.h>
