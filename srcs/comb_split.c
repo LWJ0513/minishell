b9b45239ab7cfcdb1b08ad4556868819fd812d91
임원정 <wonlim@student.42seoul.kr>
2023-02-09 20:07:52 +0900

    edit comb_split

--- srcs/comb_split.c
+++ srcs/comb_split.c
@@ -6,13 +6,13 @@
 /*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/09 17:59:47 by wonlim            #+#    #+#             */
-/*   Updated: 2023/02/09 18:00:02 by wonlim           ###   ########.fr       */
+/*   Updated: 2023/02/09 20:06:49 by wonlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-char * comb_split(char **split, int i)
+char *comb_split(char **split, int i)
 {
 	char *str;
 	char *tmp;
@@ -20,12 +20,17 @@ char * comb_split(char **split, int i)
 	str = malloc(0);
 	while (split[i])
 	{
-
 		tmp = ft_strjoin(str, split[i]);
 		if (str)
 			free(str);
-		str = ft_strjoin(tmp, " ");
-		free(tmp);
+		if (split[i + 1] != NULL)
+		{
+			str = ft_strjoin(tmp, " ");
+			free(tmp);
+		}
+		else
+			str = tmp;
+
 		i++;
 	}
 	return (str);
