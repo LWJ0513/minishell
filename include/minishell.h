57b6374dfd5c6c840b789710da1674e0fe8f1f99
임원정 <wonlim@student.42seoul.kr>
2023-02-09 18:00:47 +0900

    edit header

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 21:36:54 by wonlim            #+#    #+#             */
-/*   Updated: 2023/02/09 17:51:06 by wonlim           ###   ########.fr       */
+/*   Updated: 2023/02/09 18:00:10 by wonlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ typedef struct s_char
 void    ft_pwd();
 void    ft_cd(char *str);
 void	free_split(char **split);
+char * comb_split(char **split, int i);
 
 
 #endif
