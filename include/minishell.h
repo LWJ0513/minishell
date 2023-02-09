c4a6bf23d983fdf9c0895c0b0bdca072439384b1
임원정 <wonlim@student.42seoul.kr>
2023-02-09 16:22:05 +0900

    add function in header #8

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 21:36:54 by wonlim            #+#    #+#             */
-/*   Updated: 2023/02/09 15:31:26 by wonlim           ###   ########.fr       */
+/*   Updated: 2023/02/09 16:08:09 by wonlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,9 @@ typedef struct s_char
 {
 	
 }	t_char;
+
+void    ft_pwd();
+
+
 
 #endif
