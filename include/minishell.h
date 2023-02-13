49205cb5552e5b9f6ecbc66515fe9e8cd4f522ba
임원정 <wonlim@student.42seoul.kr>
2023-02-13 18:48:12 +0900

    edit header

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 21:36:54 by wonlim            #+#    #+#             */
-/*   Updated: 2023/02/09 21:23:05 by wonlim           ###   ########.fr       */
+/*   Updated: 2023/02/13 18:47:09 by wonlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,24 @@
 
 typedef struct s_node
 {
-	struct	s_node *next;
-	char	**command;
-	int		result;
+	int				result;
+	char			**command;
+	struct s_node	*next;
 }	t_node;
 
 typedef struct s_list
 {
-	pid_t pid;
-	t_node *head;	
+	t_node	*head;
+	pid_t	pid;
+	int		pipe_cnt;
+	int		command_cnt
 }	t_list;
 
-int    ft_pwd();
-int    ft_cd(char *str);
+int		ft_pwd(void);
+int		ft_cd(char *str);
 void	free_split(char **split);
-char * comb_split(char **split, int i);
-int	ft_strcmp(char *s1, char *s2);
+char	*comb_split(char **split, int i);
+int		ft_strcmp(char *s1, char *s2);
 
 
 #endif
