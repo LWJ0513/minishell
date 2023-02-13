ed20c3fe55adfefce474d069d772a07fbdeae3b3
임원정 <wonlim@student.42seoul.kr>
2023-02-13 22:24:08 +0900

    edit libft library
    bonus 파트 삭제

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: wonlim <wonlim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 14:53:40 by wonlim            #+#    #+#             */
-/*   Updated: 2022/12/13 14:37:21 by wonlim           ###   ########.fr       */
+/*   Updated: 2023/02/13 22:00:24 by wonlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-typedef struct s_list
-{
-	void				*content;
-	struct s_list		*next;
-}			t_list;
 
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
@@ -60,16 +55,6 @@ void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
 void		ft_striteri(char *s, void (*f)(unsigned int, char*));
-
-t_list		*ft_lstnew(void *content);
-void		ft_lstadd_front(t_list **lst, t_list *new);
-int			ft_lstsize(t_list *lst);
-t_list		*ft_lstlast(t_list *lst);
-void		ft_lstadd_back(t_list **lst, t_list *new);
-void		ft_lstdelone(t_list *lst, void (*del)(void *));
-void		ft_lstclear(t_list **lst, void (*del)(void *));
-void		ft_lstiter(t_list *lst, void (*f)(void *));
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 void		ft_putchar(char c, int *i);
 void		ft_putstr(char *s, int *i);
