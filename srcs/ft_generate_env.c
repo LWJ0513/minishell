ede3ef683d13e724158c68d430f1900f3d399203
임원정 <wonlim@student.42seoul.kr>
2023-02-14 18:10:06 +0900

    edit ft_generate_env

--- srcs/ft_generate_env.c
+++ srcs/ft_generate_env.c
@@ -1,28 +1,28 @@
 #include "../include/minishell.h"
 
-int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-	int	size;
+// int	ft_strcmp(char *s1, char *s2)
+// {
+// 	int	i;
+// 	int	size;
 
-	size = 0;
-	while (s1[size])
-		size++;
-	++size;
-	i = 0;
-	while (i < size)
-	{
-		if (s1[i] == s2[i])
-		{
-			if (s1[i] == '\0')
-				return (0);
-			i++;
-		}
-		else
-			return (s1[i] - s2[i]);
-	}
-	return (0);
-}
+// 	size = 0;
+// 	while (s1[size])
+// 		size++;
+// 	++size;
+// 	i = 0;
+// 	while (i < size)
+// 	{
+// 		if (s1[i] == s2[i])
+// 		{
+// 			if (s1[i] == '\0')
+// 				return (0);
+// 			i++;
+// 		}
+// 		else
+// 			return (s1[i] - s2[i]);
+// 	}
+// 	return (0);
+// }
 
 void    ft_sort_envp(t_envp *list)
 {
@@ -60,30 +60,30 @@ void    ft_export(t_envp *list)
     tmp = list->head;
     while(tmp)
     {
-        printf("%s=%s\n", tmp1->key, tmp1->value);
-        tmp1 = tmp1->next;
+        printf("%s=%s\n", tmp->key, tmp->value);
+        tmp = tmp->next;
+    }
+}
+
+void Insert(t_envp* list, char **tmp2, char *key1, char *value1) {
+    t_e_node* newnode = (t_e_node*)malloc(sizeof(t_e_node));
+    newnode->next = NULL;
+    newnode->prev = NULL;
+    newnode->split1=tmp2;
+    newnode->key = key1;
+    newnode->value = value1;
+    t_e_node* tmp = list->head;
+    if (tmp == NULL)
+        list->head = newnode;
+    else {
+        newnode->next = tmp;
+        tmp->prev = newnode;
+        list->head = newnode;
     }
 }
 
 t_envp	*envp_init(char **envp)
 {
-    t_envp *new_env;
-    char    **tmp;
-    int     i;
-
-    i = 0;
-    new_env = (t_envp*)malloc(sizeof(t_envp));
-    if (!new_env)
-        printf("error");
-    while (*(envp + i))
-    {
-        tmp = ft_split(*(envp + i), '=');
-        for (int k = 0;k < ft_strlen(tmp); k++)
-        {
-            printf("%s\n",(tmp + k));
-        }
-    }
-
     t_envp *new_env;
     char    **tmp;
     size_t     i;
@@ -99,9 +99,9 @@ t_envp	*envp_init(char **envp)
         Insert(new_env, tmp, tmp[0], tmp[1]);
         i++;
     }
-    t_e_node *tmp1 = new_env -> head; 
+    // t_e_node *tmp1 = new_env -> head; 
 
-    tmp1 = new_env -> head; 
+    // tmp1 = new_env -> head; 
     // while(tmp1)
     // {
     //     free(tmp1->split1);
