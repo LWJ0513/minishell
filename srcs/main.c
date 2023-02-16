5a7bf96ec130ef5ffdcc17a0940b293968998ff0
임원정 <wonlim@student.42seoul.kr>
2023-02-16 17:09:55 +0900

    명령어 앞의 예외처리 #23
    공백이나 파이프가 맨 처음에 왔을 때에 대한 예외처리

--- srcs/main.c
+++ srcs/main.c
@@ -11,10 +11,25 @@
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+char *cut_front(char *str)
+{
+
+	while (*str == ' ' || *str == '|')
+	{
+		if (*str == '|')
+		{
+			printf("zsh: parse error near `|'\n");
+			return 0;
+		}
+		str++;
+	}
+	return str;
+}
 
 int main(int argc, char **argv, char **envp)
 {
 	char *str;
+	char *line;
 	char **split_pipe;
 	t_list list;
 	t_node *node;
@@ -24,6 +39,8 @@ int main(int argc, char **argv, char **envp)
 	// int last_result;
 
 	env = envp_init(envp);
+	if (env)
+	;
 
 	if (argc != 1 || !argv)
 		exit(0);
@@ -31,16 +48,22 @@ int main(int argc, char **argv, char **envp)
 	while (1)
 	{
 		reset_list(&list);
-		str = readline("minishell $ ");
+		line = readline("minishell $ ");
 
-		if (str)
+		if (line)
 		{
+			// todo front 예외처리
+			str = cut_front(line);
+			if (!str)
+				continue;
 
+			// - pipe 기준으로 split
 			list.cnt_pipe = count_pipe(str);
 			split_pipe = ft_split(str, '|');
 
 			// todo spilt_pipe free
 
+			// - command 를 노드화
 			int i = 0;
 			while (split_pipe[i])
 			{
@@ -58,19 +81,21 @@ int main(int argc, char **argv, char **envp)
 			}
 			list.cnt_cmd = count_cmd(&list, i);
 
+			// - 예외처리 
 			if (i != list.cnt_cmd || list.cnt_pipe + 1 != list.cnt_cmd)
 			{
-				add_history(str);
+				add_history(line);
 				printf("Syntax error !\n");
 				// todo free
 				continue;
 			}
+		
 
-			// 맨 마지막에 파이프가 있으면 pipe >  출력
+			// todo 맨 마지막에 파이프가 있으면 pipe >  출력
 
 			// todo 연결 리스트 free (각각의 노드 안에 있는 node.cmd free)
 
-
+			// - command fork 뜨고 실행
 			i = 0;
 			int status;
 			while (i < list.cnt_cmd)
@@ -94,10 +119,14 @@ int main(int argc, char **argv, char **envp)
 						j++;
 					}
 					printf("\"%s\"\n", (node->cmd)[0]);
-					
+
 					// todo execute_command() 함수 구현
-					execute_command(node, &env);
-					
+					// if (!ft_strncmp(split[0], "pwd", ft_strlen(split[0])))
+					// 	last_result = ft_pwd();
+					// if (!ft_strncmp(split[0], "cd", ft_strlen(split[0])))
+					// 	last_result = ft_cd(comb_split(split, 1));
+					// execute_command(node, &env);
+
 					exit(0);
 				}
 				else
@@ -107,18 +136,13 @@ int main(int argc, char **argv, char **envp)
 					exit(0);
 				}
 			}
-
-			// if (!ft_strncmp(split[0], "pwd", ft_strlen(split[0])))
-			// 	last_result = ft_pwd();
-			// if (!ft_strncmp(split[0], "cd", ft_strlen(split[0])))
-			// 	last_result = ft_cd(comb_split(split, 1));
 		}
 		else // str = NULL 이라면 (EOF, cntl + D)
 			break;
 
 		// system("leaks minishell");
-		add_history(str);
+		add_history(line);
 		free_split(split_pipe);
-		free(str);
+		free(line);
 	}
 }
